18c8a7392aa02152f2e296ebe4da9b6a72a54788
tenzin <tenzinsonam008@gmail.com>
2017-06-07 01:45:35 +0530

    asdf is check

--- asdf/src/extension.ts
+++ asdf/src/extension.ts
@@ -18,54 +18,36 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         let editor = vscode.window.activeTextEditor
         let text = editor.document.getText()
-        let tesy = new String
-        /*
-        fs.readFile('temp.c','r',function(err, data){
-            tesy = data
-        })
-        */
-        fs.readFile('wolo.txt',function(err, data){
-            //console.log('hello')
-            tesy = data.toString('utf-8')
-            //console.log(data.toString('utf-8'))
-        })
-        let position = editor.selection.active
-        let terminal = vscode.window.createTerminal()
-        //terminal.show()
-        //terminal.sendText('cd /home/tenzin/Downloads/asdf')
-        //terminal.sendText('python test.py')
-        terminal.sendText('pwd')
-        let decs = []
-        let l = position.line + 1
-        let c = position.character + 1
-        let pos = new vscode.Position(l,c)        //let pos
-        //await
-        //pos.line = position.line + 1
-        //pos.character = position.character + 1
-        let decorationType = vscode.window.createTextEditorDecorationType({
+         let decorationType = vscode.window.createTextEditorDecorationType({
             backgroundColor: 'red'
             //borderWidth: '2px',
             //borderStyle: 'solid'
         })
-        let pos1 = new vscode.Range(1,1,1,7)
-        let pos2 = new vscode.Range(0,1,0,8)
-        decs.push(pos1)
-        decs.push(pos2)
-        function delay(ms: number){
-            return new Promise(resolve => setTimeout(resolve, ms))
-        }
+        var PythonShell = require('python-shell');
+        PythonShell.run('test.py', function (err) {
+            if (err) throw err;
+            var lineReader = require('readline').createInterface({
+                input: require('fs').createReadStream('nibba.txt')
+            });
 
-        //await delay(3000)
-        setTimeout(function(){
-            console.log('adas')
-        },3000)
+            let i = 1;
+            let decs = []  
+            lineReader.on('line', function (line) {
+                if(i%4==0){
+                    let line_split = line.split(",");
+                    console.log('Line from file:', line_split);     
+                    let pos1 = new vscode.Range(parseInt(line_split[0]),parseInt(line_split[1]),parseInt(line_split[2]),parseInt(line_split[3]));
+                    //console.log(pos1);
+                    decs.push(pos1);      
+                    editor.setDecorations(decorationType,decs);
+                }
+                i+=1;
+            });
+        });
         
-        editor.setDecorations(decorationType,decs)
-        //vscode.commands.executeCommand('workbench.action.tasks.runTask')
-        //var spawn = require("child_process").spawn
-        //var process = spawn('python',["/home/tenzin/Downloads/asdf/test.py"])
-        
-        console.log(tesy)
+        let position = editor.selection.active
+        let terminal = vscode.window.createTerminal()
+
     });
 
     context.subscriptions.push(disposable);
--- asdf/test.py
+++ asdf/test.py
@@ -1,2 +1,7 @@
-file = open('nibba.txt','w')
-file.close()
+import random
+f = open('nibba.txt','w')
+strx=''
+for i in xrange(5):
+    strx+=str(random.randint(1,9))+","+str(random.randint(1,9))+","+str(random.randint(1,9))+","+str(random.randint(1,9))+"\n"
+f.write(strx)
+f.close()
